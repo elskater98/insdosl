8b9f84fc084f6176faa80f9e76b1109b25049234
David Vigo <david.vigoesteban@gmail.com>
2022-11-26 22:41:55 +0100

    Add camera tab

---
+++ app/src/app/camera/camera.page.ts
@@ -0,0 +1,18 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-camera',
+  templateUrl: 'camera.page.html',
+  styleUrls: ['camera.page.scss']
+})
+export class CameraPage implements OnInit, OnDestroy {
+
+  ngOnInit() {
+  }
+  
+  
+  ngOnDestroy(): void {
+    
+  }
+ 
+}
