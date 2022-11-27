b91c7802c8544661482185cedf7039c651437e0f
David Vigo <david.vigoesteban@gmail.com>
2022-11-27 02:25:58 +0100

    Add camera!

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,13 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
 
-import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NativeGeocoder} from "@ionic-native/native-geocoder/ngx";
 import {Geolocation} from "@ionic-native/geolocation/ngx";
-import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
--- app/src/app/services/photo.service.ts
+++ app/src/app/services/photo.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Directory, Filesystem } from '@capacitor/filesystem';
 
 
 @Injectable({
@@ -7,13 +9,64 @@ import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera
 })
 export class PhotoService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   public async addNewToGallery() {
-    await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri, // file-based data; provides best performance
+      source: CameraSource.Camera, // automatically take a new photo with the camera
+      quality: 100 // highest quality (0 to 100)
     });
+
+    // Save the picture and add it to photo collection
+    const savedImageFile = await this.savePicture(capturedPhoto);
   }
+  
+  private async savePicture(cameraPhoto: Photo) {
+    return await this.readAsBase64(cameraPhoto);
+  }
+
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    const final = await this.convertBlobToBase64(blob);
+    this.sendPostRequest(final);
+    console.log(final);
+  }
+
+  private sendPostRequest(blob: any) {
+    var headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+
+    
+    const httpOptions = {
+      headers: new HttpHeaders({
+          'Content-Type': 'application/json; charset=utf-8',
+          'Accept-Charset': 'utf-8'
+        })
+      };
+
+    let postData = {
+      "latitude": 1.123,
+      "longitude": 2.12213,
+      "photo": blob
+    }
+
+    this.http.post("https://pondere.es:8004/rd008", postData, httpOptions)
+      .subscribe(data => {
+        console.log(data);
+       }, error => {
+        console.log(error);
+      });
+  }
+  
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
